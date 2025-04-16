7dd49e1d4eea4f10c041f608e003dfdcb7593db5
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-16 07:04:54 -0700

    updated handlers to user Mediator

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,24 +8,17 @@ using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(AppDbContext context, IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivites()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
-
-        if (activity == null)
-        {
-            return NotFound();
-        }
-
-        return activity;
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 }
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,5 +6,12 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseApiController : ControllerBase { }
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator =>
+            _mediator ?? HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator service is unavailable");
+    }
 }
