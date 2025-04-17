ba35d5cdf7883229205c2e52ef430c6fdb9dc980
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-17 06:12:33 -0700

    added delete activity endpoint and setup debugger profiles

--- Application/Activities/Commands/DeleteActivity.cs
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -1,27 +1,25 @@
 using System;
-using AutoMapper;
-using Domain;
 using MediatR;
 using Persistence;
 
 namespace Application.Activities.Commands;
 
-public class EditActivity
+public class DeleteActivity
 {
     public class Command : IRequest
     {
-        public required Activity Activity { get; set; }
+        public required string Id { get; set; }
     }
 
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command>
+    public class Handler(AppDbContext context) : IRequestHandler<Command>
     {
         public async Task Handle(Command request, CancellationToken cancellationToken)
         {
             var activity = await context.Activities
-                .FindAsync([request.Activity.Id], cancellationToken)
+                .FindAsync([request.Id], cancellationToken)
                     ?? throw new Exception("Cannot find activity");
 
-            mapper.Map(request.Activity, activity);
+            context.Remove(activity);
 
             await context.SaveChangesAsync(cancellationToken);
         }
