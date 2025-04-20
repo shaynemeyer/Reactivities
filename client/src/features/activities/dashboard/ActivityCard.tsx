b2fc7c5523d9586ec662ca0e5e124d0771776253
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-20 08:03:39 -0700

    changes to ActivityCard

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,16 +1,10 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { useActivities } from "@/lib/hooks/useActivities";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatDateForDisplay } from "@/lib/utils";
-import { useNavigate } from "react-router";
+import { Clock4, MapPin, User } from "lucide-react";
+import { Link, useNavigate } from "react-router";
 
 type Props = {
   activity: Activity;
@@ -19,39 +13,66 @@ type Props = {
 
 function ActivityCard({ activity }: Props) {
   const navigate = useNavigate();
-  const { deleteActivity } = useActivities();
+
+  const isHost = false;
+  const isGoing = false;
+  const label = isHost ? "You are hosting" : "You are going";
+  const isCancelled = false;
+  const color = isHost ? "secondary" : isGoing ? "warning" : "default";
 
   return (
     <Card className="rounded-md mb-4" key={activity.id}>
       <CardHeader>
-        <CardTitle>{activity.title}</CardTitle>
-        <CardDescription>{formatDateForDisplay(activity.date)}</CardDescription>
+        <CardTitle className="p-0">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between gap-2">
+              <Avatar className="h-15 w-15">
+                <AvatarFallback>
+                  <User className="h-12 w-12" />
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <h3>{activity.title}</h3>
+                <span className="text-xs text-gray-400">
+                  Hosted by <Link to={`/profiles/bob`}>Bob</Link>
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 mr-2">
+              {isHost && <Badge variant="secondary">Hosting</Badge>}
+              {isGoing && <Badge variant="outline">Going</Badge>}
+              {isCancelled && (
+                <Badge variant="destructive" className="rounded-xl bg-gray-100">
+                  Cancelled
+                </Badge>
+              )}
+            </div>
+          </div>
+        </CardTitle>
       </CardHeader>
-      <CardContent>
-        <p>{activity.description}</p>
-        <p>
-          {activity.city} / {activity.venue}
-        </p>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Badge variant="outline">{activity.category}</Badge>
-        <div className="flex gap-2">
-          <Button
-            size="sm"
-            onClick={() => navigate(`/activities/${activity.id}`)}
-          >
-            View
-          </Button>
-          <Button
-            size="sm"
-            variant="destructive"
-            onClick={() => deleteActivity.mutate(activity.id)}
-            disabled={deleteActivity.isPending}
-          >
-            Delete
-          </Button>
+      <hr />
+      <CardContent className="flex gap-3 text-sm">
+        <div className="flex gap-1">
+          <Clock4 />{" "}
+          <h5 className="text-gray-500 mb-3">
+            {formatDateForDisplay(activity.date)}
+          </h5>
         </div>
-      </CardFooter>
+        <div className="flex gap-1">
+          <MapPin />
+          {activity.venue}
+        </div>
+      </CardContent>
+      <CardContent className="bg-gray-100 py-3">Attendees go here</CardContent>
+      <CardContent className="flex justify-between">
+        <p>{activity.description}</p>
+        <Button
+          size="sm"
+          onClick={() => navigate(`/activities/${activity.id}`)}
+        >
+          View
+        </Button>
+      </CardContent>
     </Card>
   );
 }
