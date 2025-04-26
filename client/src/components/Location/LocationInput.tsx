3ac9a58a459cb35a7f7e83933142a92b830ab0b1
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-26 15:36:11 -0700

    added state to locationinput component

--- client/src/components/Location/LocationInput.tsx
+++ client/src/components/Location/LocationInput.tsx
@@ -1,10 +1,15 @@
-import { Input } from "../ui/input";
+import { Input } from '../ui/input';
+import { useState } from 'react';
+import { LocationIQSuggestion } from '@/lib/types';
 
 function LocationInput() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestions, setSuggestions] = useState<LocationIQSuggestion[]>([]);
+
   const locationUrl = `https://api.locationiq.com/v1/autocomplete?key=${
     import.meta.env.VITE_LOCATIONIQ_ACCESS_TOKEN
   }&limit=5&dedupe=1&`;
-  console.log(locationUrl);
+
   return <Input />;
 }
 export default LocationInput;
