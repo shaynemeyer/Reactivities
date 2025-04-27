c22d65889856676f739cfb05ca23ccfe4a3cb329
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-27 09:46:31 -0700

    added Identity and updated db

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class AppDbContext(DbContextOptions options) : DbContext(options)
+public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Activity> Activities { get; set; }
 }
