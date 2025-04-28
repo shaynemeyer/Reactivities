5f46c2c8ad56870641bf3f6363c8a574bffd3db1
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-27 19:27:26 -0700

    added login form

--- client/src/features/account/LoginForm.tsx
+++ client/src/features/account/LoginForm.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
 import {
+  Form,
   FormControl,
   FormField,
   FormItem,
@@ -10,7 +12,7 @@ import { Input } from '@/components/ui/input';
 import { useAccount } from '@/lib/hooks/useAccount';
 import { loginSchema, LoginSchema } from '@/lib/schemas/loginSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 function LoginForm() {
   const { loginUser } = useAccount();
@@ -30,41 +32,49 @@ function LoginForm() {
   };
 
   return (
-    <div>
-      <Form {...form}>
-        <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="Title" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input placeholder="Password" {...field} type="password" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    <div className="flex items-center justify-center mt-60">
+      <Card className="w-[500px]">
+        <CardContent>
+          <Form {...form}>
+            <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        {...field}
+                        type="password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-          <Button type="submit" size="sm">
-            Login
-          </Button>
-        </form>
-      </Form>
+              <Button type="submit" size="sm" className="w-full">
+                Login
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
