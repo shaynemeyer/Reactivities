fe72113d5b836384577195d8314995249e2c59a1
Shayne Meyer <shayne.meyer@gmail.com>
2025-05-05 12:38:34 -0700

    Added Get Profile feature

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,5 +1,6 @@
 using System;
 using Application.Profiles.Commands;
+using Application.Profiles.DTOs;
 using Application.Profiles.Queries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -24,5 +25,17 @@ public class ProfilesController : BaseApiController
     public async Task<ActionResult> DeletePhoto(string photoId)
     {
         return HandleResult(await Mediator.Send(new DeletePhoto.Command { PhotoId = photoId }));
+    }
+
+    [HttpPut("{photoId}/setMain")]
+    public async Task<ActionResult> SetMainPhoto(string photoId)
+    {
+        return HandleResult(await Mediator.Send(new SetMainPhoto.Command { PhotoId = photoId }));
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<ActionResult<UserProfile>> GetProfile(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfile.Query { UserId = userId }));
     }
 }
