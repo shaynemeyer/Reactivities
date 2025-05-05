b743067ccdeed3acaa26ba38fd5a1b1266d5234c
Shayne Meyer <shayne.meyer@gmail.com>
2025-05-05 11:19:01 -0700

    added profiles controller

---
+++ API/Controllers/ProfilesController.cs
@@ -0,0 +1,15 @@
+using System;
+using Application.Profiles.Commands;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ProfilesController : BaseApiController
+{
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<Photo>> AddPhoto(IFormFile file)
+    {
+        return HandleResult(await Mediator.Send(new AddPhoto.Command { File = file }));
+    }
+}
